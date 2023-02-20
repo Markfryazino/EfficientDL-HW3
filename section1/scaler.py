1fc5f5f7bc07aa3b472c4f8cd5e31eacc1a307f5
Markfryazino <rofinmp@gmail.com>
2023-02-20 17:16:15 +0100

    initial

---
+++ section1/scaler.py
@@ -0,0 +1,60 @@
+import torch
+import logging
+import wandb
+
+
+class CustomGradScaler:
+    def __init__(self, init_scale=65536.0, growth_factor=2.0, backoff_factor=0.5, 
+                 growth_interval=2000, fixed=False):
+        self.scale_factor = init_scale
+        self.growth_factor = growth_factor
+        self.backoff_factor = backoff_factor
+        self.growth_interval = growth_interval
+
+        self.fixed = fixed
+        # флаг, который помогает вызывать scale и unscale_ по-очереди 
+        self.scaled_state = False
+        # флаг про то, нашли ли мы в unscale_ плохие значения в градиентах
+        self.found_bad_param = False
+        # количество последовательных нескипнутых итераций обучения
+        self.num_unskipped = 0
+
+    def scale(self, outputs: torch.Tensor) -> torch.Tensor:
+        if self.scaled_state:
+            return outputs
+        else:
+            self.scaled_state = True
+            self.found_bad_param = False
+            return outputs * self.scale_factor
+
+    def unscale_(self, optimizer: torch.optim.Optimizer) -> None:
+        if self.scaled_state:
+            self.scaled_state = False
+            for group in optimizer.param_groups.values():
+                for param in group:
+                    param.grad /= self.scale_factor
+                    self.found_bad_param = self.found_bad_param or not torch.isfinite(param.grad)
+    
+    def step(self, optimizer: torch.optim.Optimizer) -> None:
+        self.unscale_(optimizer)
+
+        if not self.found_bad_param:
+            optimizer.step()
+        else:
+            self.num_skipped
+    
+    def update(self) -> None:
+        wandb.log({"section1/gradient scaling factor": self.scale_factor}, step=wandb.step)
+
+        # если используем статический скейлинг, то просто ничего не делаем
+        if self.fixed:
+            return
+
+        if self.found_bad_param:
+            self.scale_factor *= self.backoff_factor
+            self.num_unskipped = 0
+        else:
+            self.num_unskipped += 1
+            if self.num_unskipped == self.growth_interval:
+                self.scale_factor *= self.growth_factor
+                self.num_unskipped = 0
---
+++ section1/train.py
@@ -0,0 +1,95 @@
+import torch
+import hydra
+import wandb
+import omegaconf
+
+from hydra.utils import instantiate
+from torch import nn
+from tqdm.auto import tqdm
+from typing import Union
+
+from unet import Unet
+
+from dataset import get_train_data
+from scaler import CustomGradScaler
+
+
+def zeros_num_in_grad(optimizer):
+    num_zeros = 0
+    for group in optimizer.param_groups.values():
+        for param in group:
+            num_zeros += (param.grad == 0).sum()
+    return num_zeros
+
+
+def train_epoch(
+    train_loader: torch.utils.data.DataLoader,
+    model: torch.nn.Module,
+    criterion: torch.nn._Loss,
+    optimizer: torch.optim.optimizer.Optimizer,
+    scaler: Union[torch.cuda.amp.GradScaler, CustomGradScaler],
+    fp16: bool,
+    device: torch.device,
+    log_steps: int
+) -> None:
+    model.train()
+
+    pbar = tqdm(enumerate(train_loader), total=len(train_loader))
+
+    logging_loss = logging_zeros = logging_accuracy = 0
+    for i, (images, labels) in pbar:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        with torch.cuda.amp.autocast(enabled=fp16):
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        logging_zeros += zeros_num_in_grad(optimizer) / log_steps
+        optimizer.zero_grad()
+
+        accuracy = ((outputs > 0.5) == labels).float().mean()
+        logging_loss += loss.item() / log_steps
+        logging_accuracy += accuracy / log_steps
+
+        pbar.set_description(f"Loss: {round(loss.item(), 4)} " f"Accuracy: {round(accuracy.item() * 100, 4)}")
+        wandb.log({
+            "section1/loss": logging_loss,
+            "section1/accuracy": logging_accuracy,
+            "section1/zero number in gradients": logging_zeros
+        })
+
+        if i % log_steps == 0:
+            logging_loss = logging_zeros = logging_accuracy = 0
+
+
+@hydra.main(version_base=None, config_path="conf", config_name="config")
+def train(cfg):
+    config = omegaconf.OmegaConf.to_container(
+        cfg, resolve=True, throw_on_missing=True
+    )
+    wandb.init(entity=cfg.wandb.entity, project=cfg.wandb.project, name=cfg.wandb.name, config=config)
+
+    device = torch.device(cfg.device)
+    model = Unet().to(device)
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    scaler = instantiate(cfg.scaling)
+
+    # проверка, что не будет обучения в fp32 со скейлингом
+    assert cfg.fp16 or not scaler.enabled
+
+    train_loader = get_train_data()
+
+    num_epochs = 5
+    for epoch in range(0, num_epochs):
+        train_epoch(train_loader, model, criterion, optimizer, scaler, cfg.fp16, device=device, log_steps=cfg.wandb.log_steps)
+
+    wandb.finish()
+
+
+if __name__ == "__main__":
+    train()
